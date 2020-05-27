9bd2f79e7db5fae67f5f59734eb8a68b22a705c7
Ginnio Sarabia <sarabia.seven@gmail.com>
2020-05-26 20:03:18 -0500

    Updates

---
+++ precache-manifest.a75ae8a4ed59776825db15f5698e25c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f64e73b5568c2da9d33f3610ea72b23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c00863d7f7a7f96417c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c00863d7f7a7f96417c",
+    "url": "/static/js/main.1d86df82.chunk.js"
+  },
+  {
+    "revision": "9c72d3d3169ebbf433e6",
+    "url": "/static/js/runtime-main.dafc8f45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
